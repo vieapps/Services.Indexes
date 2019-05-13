e090f97e48ec5f1534f81bad1e532390c994eda9
Quynh Nguyen <mr.quynh@gmail.com>
2019-05-14 03:08:50 +0700

    Improvement: cancellation token

--- Main.cs
+++ Main.cs
@@ -43,43 +43,44 @@ namespace net.vieapps.Services.Indexes
 			// process
 			var stopwatch = Stopwatch.StartNew();
 			this.WriteLogs(requestInfo, $"Begin request ({requestInfo.Verb} {requestInfo.GetURI()})");
-			try
-			{
-				JToken json = null;
-				switch (requestInfo.ObjectName.ToLower())
+			using (var cts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken, this.CancellationTokenSource.Token))
+				try
 				{
-					case "rates":
-					case "exchangerates":
-					case "exchange-rates":
-						json = await this.ProcessExchangeRatesAsync(requestInfo, cancellationToken).ConfigureAwait(false);
-						break;
+					JToken json = null;
+					switch (requestInfo.ObjectName.ToLower())
+					{
+						case "rates":
+						case "exchangerates":
+						case "exchange-rates":
+							json = await this.ProcessExchangeRatesAsync(requestInfo, cts.Token).ConfigureAwait(false);
+							break;
 
-					case "stock":
-					case "stockquote":
-					case "stockquotes":
-					case "stock-quote":
-					case "stock-quotes":
-						json = string.IsNullOrWhiteSpace(requestInfo.GetObjectIdentity())
-							? await this.ProcessStockIndexesAsync(requestInfo, cancellationToken).ConfigureAwait(false)
-							: await this.ProcessStockQuoteAsync(requestInfo, cancellationToken).ConfigureAwait(false);
-						break;
+						case "stock":
+						case "stockquote":
+						case "stockquotes":
+						case "stock-quote":
+						case "stock-quotes":
+							json = string.IsNullOrWhiteSpace(requestInfo.GetObjectIdentity())
+								? await this.ProcessStockIndexesAsync(requestInfo, cts.Token).ConfigureAwait(false)
+								: await this.ProcessStockQuoteAsync(requestInfo, cts.Token).ConfigureAwait(false);
+							break;
 
-					default:
-						throw new InvalidRequestException($"The request is invalid [({requestInfo.Verb}): {requestInfo.GetURI()}]");
+						default:
+							throw new InvalidRequestException($"The request is invalid [({requestInfo.Verb}): {requestInfo.GetURI()}]");
+					}
+					stopwatch.Stop();
+					this.WriteLogs(requestInfo, $"Success response - Execution times: {stopwatch.GetElapsedTimes()}");
+					if (this.IsDebugResultsEnabled)
+						this.WriteLogs(requestInfo,
+							$"- Request: {requestInfo.ToJson().ToString(this.IsDebugLogEnabled ? Newtonsoft.Json.Formatting.Indented : Newtonsoft.Json.Formatting.None)}" + "\r\n" +
+							$"- Response: {json?.ToString(this.IsDebugLogEnabled ? Newtonsoft.Json.Formatting.Indented : Newtonsoft.Json.Formatting.None)}"
+						);
+					return json;
 				}
-				stopwatch.Stop();
-				this.WriteLogs(requestInfo, $"Success response - Execution times: {stopwatch.GetElapsedTimes()}");
-				if (this.IsDebugResultsEnabled)
-					this.WriteLogs(requestInfo,
-						$"- Request: {requestInfo.ToJson().ToString(this.IsDebugLogEnabled ? Newtonsoft.Json.Formatting.Indented : Newtonsoft.Json.Formatting.None)}" + "\r\n" +
-						$"- Response: {json?.ToString(this.IsDebugLogEnabled ? Newtonsoft.Json.Formatting.Indented : Newtonsoft.Json.Formatting.None)}"
-					);
-				return json;
-			}
-			catch (Exception ex)
-			{
-				throw this.GetRuntimeException(requestInfo, ex, stopwatch);
-			}
+				catch (Exception ex)
+				{
+					throw this.GetRuntimeException(requestInfo, ex, stopwatch);
+				}
 		}
 
 		#region Exchange rates
